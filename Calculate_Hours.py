acf3202e8c522739b2f3a35a40e3e5ba7460f498
Wonwoo Jeong <warrenww14@gmail.com>
2024-09-25 23:25:11 -0400

    Added some comments

--- Calculate_Hours.py
+++ Calculate_Hours.py
@@ -2,6 +2,12 @@ import sys
 import time
 import datetime
 
+'''
+course_name:start_time-end_time;
+220:10:00-12:30;
+220 Grading : 10:00 - 12:30 ;
+'''
+
 course_1 = sys.argv[2]
 course_2 = sys.argv[3]
 print("First course: ", course_1)
@@ -13,28 +19,26 @@ with open(sys.argv[1], 'r') as timesheet_file:
         timesheet += line
 
 # print(timesheet) 
-something1 = datetime.timedelta(0)
-something2 = datetime.timedelta(0)
+course_1_time = datetime.timedelta(0)
+course_2_time = datetime.timedelta(0)
 timesheet= timesheet.split(";")
 del timesheet[-1]
 for line in timesheet:
-    print(line)
+    #print(line)
     
     line = line.split(":", 1)
     course = line[0]
     start = datetime.datetime.strptime(line[1].split("-")[0].lstrip(),"%H:%M")
     end = datetime.datetime.strptime(line[1].split("-")[1].rstrip(),"%H:%M")
-    if end < start:
+    if end < start: # Prevents timedelta to go negative
         end += datetime.timedelta(days=1)
     if course.__contains__(course_1):
-       something1 += end - start 
-       print(something1)
+       course_1_time += end - start 
 
     elif course.__contains__(course_2):
-       something2 += end - start 
-       print(something2)
+       course_2_time += end - start 
     
 
-print(course_1, ": ",something1)
-print(course_2, ": ",something2)
-print(course_1, " + ", course_2, ": ", something1 + something2)
+print(course_1, ":",course_1_time)
+print(course_2, ":",course_2_time)
+print(course_1, "+", course_2, ":", course_1_time + course_2_time)
