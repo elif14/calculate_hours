53ff3dfceb13dfdf663bebea3bf1900f461538cb
Wonwoo Jeong <warrenww14@gmail.com>
2024-11-20 20:31:04 -0500

    Now it prints the line if there was an error parsing that line

--- Calculate_Hours.py
+++ Calculate_Hours.py
@@ -25,19 +25,22 @@ timesheet= timesheet.split(";")
 del timesheet[-1]
 for line in timesheet:
     #print(line)
-    
-    line = line.split(":", 1)
-    course = line[0]
-    start = datetime.datetime.strptime(line[1].split("-")[0].lstrip(),"%H:%M")
-    end = datetime.datetime.strptime(line[1].split("-")[1].rstrip(),"%H:%M")
-    if end < start: # Prevents timedelta to go negative
-        end += datetime.timedelta(days=1)
-    if course.__contains__(course_1):
-       course_1_time += end - start 
+    try:    
+        line = line.split(":", 1)
+        course = line[0]
+        start = datetime.datetime.strptime(line[1].split("-")[0].lstrip(),"%H:%M")
+        end = datetime.datetime.strptime(line[1].split("-")[1].rstrip(),"%H:%M")
+        if end < start: # Prevents timedelta to go negative
+            end += datetime.timedelta(days=1)
+        if course.__contains__(course_1):
+           course_1_time += end - start 
 
-    elif course.__contains__(course_2):
-       course_2_time += end - start 
-    
+        elif course.__contains__(course_2):
+           course_2_time += end - start 
+    except Exception as e:
+        print("ERROR in", line)
+        print("ERROR MESSAGE", e)
+        
 
 print(course_1, ":",course_1_time)
 print(course_2, ":",course_2_time)
